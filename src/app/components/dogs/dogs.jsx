675066f7c1c4a829524b96ca60cbbb1e46c46ab6
DESKTOP-I8L20FF\Miriam <asimiriam@gmail.com>
2021-05-03 23:42:33 +0200

    Components

--- src/app/components/dogs/dogs.jsx
+++ src/app/components/dogs/dogs.jsx
@@ -1,56 +1,16 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import DogsServices from '../../service/dogsServices';
-import SelectComponent from '../common/selectComponent';
-import Loader from 'react-loader-spinner';
 import Toast from 'react-toast-component';
-import { setToastMessage, setIsOpen, setBreed, setPhotosDogs} from '../../slice';
+import DogCard from './dogCard';
+import { setIsOpen } from '../../slice';
 
 import './dogs.scss';
 
 export default function Dogs() {
-    const {titleToast, messageToast, isOpen} = useSelector(store => store.toast);
-    const {breed, photosDogs} = useSelector(store => store.dogs);
-
-    const [loading, setLoading] = useState(false);
-    const [dogs, setDogs] = useState([]);
+    const { titleToast, messageToast, isOpen } = useSelector(store => store.toast);
+    const { photosDogs } = useSelector(store => store.dogs);
 
     const dispatch = useDispatch();
-
-    const getDogs = () => {
-        setLoading(true);
-        DogsServices.getBreedsDogs().then((result) => {
-            setDogs(Object.entries(result.message));
-            setLoading(false);
-        }).catch((error) => {
-            dispatch(setToastMessage({
-                title: error.responseJSON.status,
-                message: error.responseJSON.message
-            }));
-            dispatch(setIsOpen(true));
-            setLoading(false);
-        });
-    }
-
-    const getPhotosDogs = (dataBreed) => {
-        setLoading(true);
-        DogsServices.getPhotosBreed(dataBreed).then((result) => {
-            dispatch(setPhotosDogs(result.message));
-            setLoading(false);
-        }).catch((error) => {
-            dispatch(setToastMessage({
-                title: error.responseJSON.status,
-                message: error.responseJSON.message
-            }));
-            dispatch(setIsOpen(true));
-            setLoading(false);
-        });
-    }
-
-    useEffect(() => {
-        setLoading(true);
-        getDogs();
-    }, []);
 
     const renderPhotosDogs = () => {
         const list = photosDogs.map((photo, index) => {
@@ -62,20 +22,6 @@ export default function Dogs() {
         });
         return list;
     };
-
-    const onChangeBreed = (breedSeleted) => {
-        dispatch(setPhotosDogs([]));
-        const dogSelected = dogs[breedSeleted.value];
-        dispatch(setBreed(dogSelected));
-        if (dogSelected[1].length === 0) {
-            getPhotosDogs(dogSelected[0]);
-        }
-    }
-
-    const onChangeTypeBreed = (typeBreedSelected) => {
-        const dataBreed = `${breed[0]}\/${typeBreedSelected.value}`;
-        getPhotosDogs(dataBreed);
-    }
 
     const closeToastMessage = () => {
         dispatch(setIsOpen(false));
@@ -92,48 +38,13 @@ export default function Dogs() {
                 duration={10000}
                 classNames={['error']}
             />
-
-            <div id="dog-card">
-                <div id="div-title-card-dogs"> Dogs </div>
-                <div id="div-info-text-card-dogs">Selecciona la raza de perro de la que deseas visualizar las imagenes.</div>
-                <div className="div-breed-selects">
-                    <div className="div-select-component">
-                        <SelectComponent
-                            id="breed-select-component"
-                            label="Raza"
-                            options={dogs ? dogs.map((el, index) => ({ label: el[0], value: index })) : []}
-                            onChange={onChangeBreed}
-                        />
-                    </div>
-                    <div className="div-select-component">
-                        {breed && breed[1].length > 0 && (<SelectComponent
-                            id="type-breed-select-component"
-                            label="Tipo"
-                            options={breed ? breed[1].map((el) => ({ label: el, value: el })) : []}
-                            onChange={onChangeTypeBreed}
-                        />)}
-                    </div>
-                </div>
-            </div>
+            <DogCard />
             {photosDogs && (
                 <div className="photos-dogs">
                     {renderPhotosDogs()}
                 </div>
             )}
-            <Loader
-                type="Oval"
-                color="#2E4053"
-                height={80}
-                width={80}
-                visible={loading}
-            />
         </React.Fragment>
     );
 }
 
-
-Dogs.propTypes = {
-};
-
-Dogs.defaultProps = {
-};
